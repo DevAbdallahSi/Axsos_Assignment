a3fa792f394d49296bb40ef20d6da8fb8918bbaa
DevAbdallahSi <Abdallah.Salhi@axsos.academy>
2025-03-08 16:49:22 +0200

    :tet

---
+++ WEB-FUNDIMANTAL/java-js/javatest.js
@@ -0,0 +1,35 @@
+// for(var i=1 ; i <= 20 ; i++){
+//     if(i % 2 == 1){
+//         console.log(i +" "+ "odd")
+//     }
+//     else{
+//         console.log("num is not odd " + i )
+//     }
+// }
+
+// for(var dnum=99 ; dnum >= 0 ; dnum--){
+    // console.log(dnum)
+    // if(dnum % 3 == 0){
+        // console.log(dnum + " "+ "num is divisible")
+    // }
+    // else{
+    //     console.log("num is not divisible")
+    // }
+// }
+
+// function arr=[4,2.5,1,-0.5,-2,-3.5]
+// for(var x=0 ; x <= arr.length ; x++ ){
+//     console.log(x)
+// }
+
+function sum
+for( var i=1 ; i <= 100 ; i++){
+    
+sum = i+1
+console.log("sum")
+}
+
+
+
+
+
