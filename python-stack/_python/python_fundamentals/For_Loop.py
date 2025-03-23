4ddb6c1be0fb3474f207b54991b23906039b09a6
DevAbdallahSi <abdallah.falt@gmail.com>
2025-03-23 21:21:52 +0200

    up

---
+++ python-stack/_python/python_fundamentals/For_Loop.py
@@ -0,0 +1,39 @@
+# for i in range (0 , 151 , 1):  #q1
+#     print (i)
+
+
+# for i in range ( 0 , 1001 , 5 ):  #q2
+#     print(i)
+
+# for i in range( 1 , 101 , 1):  #q3
+#     if i % 5 !=0:
+#         print (i)
+#     if i % 10 == 0 :
+#         print("coding dojo")
+# ///////////////////////////////////continue
+#     elif i % 5 == 0://////////////////// if
+#         print("coding")
+
+
+# ///////////////////q4
+# sum=0    
+# count=0
+# for i in range ( 0 , 500001 , 1):
+#     if i %2 !=0:
+#         sum +=i
+#         count+=1
+# print (sum, count)
+
+# ////////////////q5
+# for i in range ( 2018 , 0 , -4):
+#     print(i)
+
+
+# /////////////////////////q6
+# lowNum =2
+# highNum=9
+# mult=3
+# for i in range ( 2 , 20 , 1):
+#     if i % 3 == 0:
+#         print(i)
+
