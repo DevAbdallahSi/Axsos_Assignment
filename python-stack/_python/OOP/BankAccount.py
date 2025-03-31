8fe3e648d12b86dbd9095cabf46871e7c88741b6
DevAbdallahSi <abdallah.falt@gmail.com>
2025-03-31 20:12:45 +0200

    classss

---
+++ python-stack/_python/OOP/BankAccount.py
@@ -0,0 +1,27 @@
+class BankAccount:
+
+    def __init__(self,int_rate, balance):
+        self.int_rate=int_rate
+        self.balance=balance
+
+    def deposit(self, amount):
+        self.balance+=amount
+        return self
+    def withdraw(self, amount):
+        self.balance-=amount
+        return self
+    def display_account_info(self):
+        print(" " +str(self.balance)) 
+        return self       
+    def yield_interest(self):
+        self.balance = self.balance*(self.int_rate/100)
+        return self
+    
+AccountA=BankAccount(2,1000)
+AccountB=BankAccount(2,5000)
+
+AccountA.deposit(2000).deposit(1500).deposit(5000).withdraw(2000).yield_interest().display_account_info()
+AccountB.deposit(1000).deposit(7000).withdraw(1000).withdraw(800).withdraw(2000).withdraw(1000).yield_interest().display_account_info()
+
+
+
