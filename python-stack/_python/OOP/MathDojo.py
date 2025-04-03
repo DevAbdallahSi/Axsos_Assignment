376840b022361f32f7ecd11e74deb8bc0d744c23
DevAbdallahSi <abdallah.falt@gmail.com>
2025-04-03 19:57:48 +0200

    done

--- python-stack/_python/OOP/MathDojo.py
+++ python-stack/_python/OOP/MathDojo.py
@@ -1,19 +1,18 @@
 import unittest
 
-# MathDojo class with add and subtract methods
 class MathDojo:
-    def __init__(self,result):
+    def __init__(self,result=0):
         self.result = result
     
     def add(self, num, *nums):
-        self.result += num  # Add the first number
-        for i in range(len(nums)):  # Add each additional number
+        self.result += num 
+        for i in range(len(nums)):  
             self.result += nums[i]
         return self
     
     def subtract(self, num, *nums):
-        self.result -= num  # Subtract the first number
-        for i in range(len(nums)):  # Subtract each additional number
+        self.result -= num  
+        for i in range(len(nums)):  
             self.result -= nums[i]
         return self
 
@@ -22,25 +21,23 @@ md.add(3,3)
 md.subtract(2)
 print(md.result)
 # Test cases for the MathDojo class
-# class sumTest(unittest.TestCase):
-    # def setUp(self):
-    #     """This method will run before each test."""
-    #     self.md = MathDojo()  # Create a fresh instance of MathDojo for each test
+class sumTest(unittest.TestCase):
+    def setUp(self):
+        self.md = MathDojo()  
 
-    # def test_add(self):        
-    #     self.md.add(5, -6, 5)  # Add 5, -6, and 5
-    #     self.assertEqual(self.md.result, 4)  # The expected result is 4
+    def test_add(self):        
+        self.md.add(5, -6, 5)  
+        self.assertEqual(self.md.result, 4) 
 
-    # def test_subtract(self):
-    #     self.md.subtract(5, -6, 5)  # Subtract 5, -6, and 5
-    #     self.assertEqual(self.md.result, -4)  # The expected result is -4
+    def test_subtract(self):
+        self.md.subtract(5, -6, 5) 
+        self.assertEqual(self.md.result, -4)  
 
-    # def test_combined_operations(self):
-    #     """Testing combined add and subtract operations"""
-    #     result = self.md.add(2, 2).add(2, 5, 1).add(3, 3, 1).subtract(3, 2).result
-    #     self.assertEqual(result, 14)  # The expected result is 5
+    def test_combined_operations(self):
+        result = self.md.add(2, 2).add(2, 5, 1).add(3, 3, 1).subtract(3, 2).result
+        self.assertEqual(result, 14)  
 
-# if __name__ == '__main__':
-#     unittest.main()  # This runs all the test cases
+if __name__ == '__main__':
+    unittest.main()  
 
 
