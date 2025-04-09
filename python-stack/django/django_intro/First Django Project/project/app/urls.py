341403cb383805ec45654dac90f80ce358768f38
DevAbdallahSi <abdallah.falt@gmail.com>
2025-04-09 15:13:05 +0200

    up

--- python-stack/django/django_intro/First Django Project/project/app/urls.py
+++ python-stack/django/django_intro/First Django Project/project/app/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.root, name='root'),
-    path('index/', views.index, name='blogs')
+    path('' , views.root),
+    path('blogs/', views.index),
+    path('blogs/new/', views.new),
+    path('blogs/creat/', views.creat),
+    path('blogs/<int:number>/', views.show),
+    path('blogs/<int:number>/edit', views.edit),
+    path('blogs/<int:number>/delete', views.destroy),
+    path('blogs/jason' , views.jason)
+
+
+
 
 ]
--- python-stack/django/django_intro/First Django Project/project/app/views.py
+++ python-stack/django/django_intro/First Django Project/project/app/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import redirect , render ,HttpResponse
+from django.shortcuts import redirect , render ,HttpResponse 
+from django.http import JsonResponse
 
 
 def root(request):   
-    return render( request, 'blogs.html')
+    return redirect('/blogs')
 
 
 def index(request):
-    return HttpResponse('hello')
+    return HttpResponse('placeholder to display a new form to create a new blog')
+def new (request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+def creat(request):
+    return redirect ('/')
+def show(request,number):
+    return HttpResponse(f" placeholder to display a new form to create a new blog {number}")
+def edit(request,number):
+    return HttpResponse(f"placeholder to display a new form to create a new blog",{number})
+def destroy(request,number):
+        return  redirect('/blogs')
+def jason(request):
+    return JsonResponse('/',safe=False)
