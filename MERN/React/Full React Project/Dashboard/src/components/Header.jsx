a6e2d0e16989bfc1cd96f0e8e8ee1b04c61cc9d9
DevAbdallahSi <abdallah.falt@gmail.com>
2025-07-17 08:35:07 +0300

    react training

---
+++ MERN/React/Full React Project/Dashboard/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { IconName } from "react-icons/";
+
+const Header = () => {
+    return (
+        <>
+          <div></div>
+        </>
+    )
+}
+export default Header;
---
+++ MERN/React/Full React Project/Dashboard/src/components/SideNav.jsx
@@ -0,0 +1,19 @@
+
+
+
+const Nav = () => {
+    return (
+        <>
+          <div>
+                <label className="burger" htmlFor="burger">
+                    <input type="checkbox" id="burger" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+                
+            </div>
+            <div></div>
+        </>
+    )
+}
