273003f6f0c6710add23c4de9d6334e16b8554b0
DevAbdallahSi <abdallah.falt@gmail.com>
2025-07-19 14:39:17 +0300

    socket

--- MERN/React/Full React Project/Dashboard/src/components/Header.jsx
+++ MERN/React/Full React Project/Dashboard/src/components/Header.jsx
@@ -1,9 +1,18 @@
-import { IconName } from "react-icons/";
+import { CiCloudSun, CiSearch, CiSettings, CiSun, CiUser } from "react-icons/ci";
+import { MdCircleNotifications } from "react-icons/md";
 
 const Header = () => {
     return (
         <>
-          <div></div>
+            <header style={{ display: "flex" }} >
+          
+                <h1><CiSearch /></h1>
+                <h1><CiCloudSun /></h1>
+                <h1><CiSettings /></h1>
+                <h1><MdCircleNotifications /></h1>
+                <h1><CiUser /></h1>
+
+        </header >
         </>
     )
 }
--- MERN/React/Full React Project/Dashboard/src/components/SideNav.jsx
+++ MERN/React/Full React Project/Dashboard/src/components/SideNav.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
+function Nav() {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-const Nav = () => {
     return (
         <>
-          <div>
-                <label className="burger" htmlFor="burger">
-                    <input type="checkbox" id="burger" />
+            <Button className="burger" onClick={handleShow}>
+                 
+                    <input type="checkbox"  />
                     <span></span>
                     <span></span>
                     <span></span>
-                </label>
-                
-            </div>
-            <div></div>
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
         </>
-    )
+    );
 }
+
+export default Nav;
